9543bc8d81b263cd2c3e8db11a68797e2a8b8e76
eliglezz <eliglezz@users.noreply.github.com>
2021-11-28 15:19:28 -0800

    updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,6 @@ const FILES_TO_CACHE = [
   "db.js",
   "index.js",
   "/manifest.webmanifest",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
