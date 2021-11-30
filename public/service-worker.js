6ec258c42d0d4c67ff35f0c533534979936fa0fe
eliglezz <eliglezz@users.noreply.github.com>
2021-11-30 13:43:25 -0800

    should work

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,8 @@ const FILES_TO_CACHE = [
   "db.js",
   "index.js",
   "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = "static-cache-v2";
